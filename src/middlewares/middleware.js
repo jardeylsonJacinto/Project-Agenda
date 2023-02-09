231a6f951de92104800d53d2d8a162fd7e791fb0
Jardeylson jacinto <jardeylsong.m@gmail.com>
2023-02-09 16:16:12 -0300

    feat: add middleware

--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -1,6 +1,7 @@
 exports.middlewareGlobal = (req, res, next) => {
   res.locals.errors = req.flash('errors');
   res.locals.success = req.flash('success');
+  res.locals.user = req.session.user;
   next();
 };
 
@@ -12,10 +13,21 @@ exports.checkCsrfError = (err, req, res, next) => {
   if(err) {
     return res.render('404');
   }
+
   next();
 };
 
 exports.csrfMiddleware = (req, res, next) => {
   res.locals.csrfToken = req.csrfToken();
+  next();
+};
+
+exports.loginRequired = (req, res, next) => {
+  if(!req.session.user) {
+    req.flash('errors', 'Você precisa fazer login.');
+    req.session.save(() => res.redirect('/'));
+    return;
+  }
+
   next();
 };
